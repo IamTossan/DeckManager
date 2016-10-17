c7146ca1e88c7111ecb7468990d91c0b54061dec
IamTossan <Tossanmusic@gmail.com>
2016-10-17 17:29:33 +0200

    optimisation des images/load time + rework checkboxes + css refait pour chrome avec script de detection de navigateur

---
+++ app/browserDetect.js
@@ -0,0 +1,26 @@
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+// At least Safari 3+: "[object HTMLElementConstructor]"
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+linkElt = document.createElement('link');
+linkElt.setAttribute('rel', "stylesheet");
+linkElt.setAttribute('typel', "text/css");
+
+
+
+if (isOpera || isFirefox || isSafari || isIE || isEdge || isChrome || isBlink) {
+  linkElt.setAttribute("href", "content/css/style.css");
+}
+
+document.head.appendChild(linkElt);
